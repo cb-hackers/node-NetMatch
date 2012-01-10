feeac70585e6fc8b916df25418d87f0bd1875342
Ville Lahdenvuo <tuhoojabotti@gmail.com>
2012-01-10 06:52:30 +0200

    GSS mega-commit. Almost finished.

--- gss/GSS.js
+++ gss/GSS.js
@@ -1,59 +1,74 @@
-var static = require('node-static')
-  , dns  = require('dns')
-  , hogan = require('hogan.js')
-  , fs   = require('fs')
-  , udp  = require('dgram')
+
+  // Oma loggeri ja join-funktio
+var Logger = require('./Logger').Logger
+  , join   = require('../src/Utils').join
+  // Pari Noden ydinmoduulia
+  , util = require('util')
   , http = require('http')
-  , join = require('../src/Utils').join
+  , udp  = require('dgram')
   , url  = require('url')
+  , dns  = require('dns')
+  , fs   = require('fs')
+  // Sekä pari herkkua NPM:stä :)
+  , async = require('async')
+  , hogan = require('hogan.js')
+  , assets = require('paperboy')
+  // Substack rulz<3
   , argv = require('optimist')
-    .default({d: false, a: '0.0.0.0', p: 31479})
-    .alias({'p' : 'port', 'a' : 'address', 'd' : 'debug', 'h': 'help'})
+    .default({d: false, a: '0.0.0.0', p: 31479, ua: 'lakka.kapsi.fi', up: 61902})
+    .alias({'h': 'help', 'p' : 'port', 'a' : 'address', 'up': 'udpport', 'ua': 'udpaddress'})
     .describe({
-      'h': 'Shows this help and exits.',
-      'p': 'Port to listen to.',
-      'a': 'Address to bind to defaults to all addresses.',
-      'd': 'Spam a lot.'})
+      'h':  'Shows this help and exits.',
+      'p':  'Port to listen to.',
+      'a':  'Address to bind to defaults to all addresses.',
+      'up': 'UDP port for listening to PING packets.',
+      'ua': 'UDP address for listening to PING packets.'})
     .usage('Run Game Server Service (GSS) -server: $0')
     .demand('p')
-    .check(function (a) {return !a.h;}) // Näytetään helppi, jos -h tai --help
-    .argv; // Palautetaan parametrit opjektina, jotta niitä
+    .check(function (a) {return !a.h;}).argv; // Näytetään helppi, jos -h tai --help
 
+// tätä profiilia vaaditaan.
 var NM_PROFILE = 'NetMatch';
 
-function GSS(p, a) {
+function GSS(p, a, up, ua) {
   var self = this;
-  self.listingTempl = hogan.compile(fs.readFileSync('listing.html', 'utf8'));
-  self.servers = [];
-  self.sock = udp.createSocket('udp4');
-  self.sock.bind(61902, 'lakka.kapsi.fi');
 
+  // Yleinen logitus
+  self.log = new Logger('gss');
+
+  // Valmistele sapluuna, jolla serverilistaus näytetään html-muodossa.
+  self.listingTempl = hogan.compile(fs.readFileSync('listing.html', 'utf8'));
+
+  // Lista palvelimista
+  self.servers = [];
+
+  // Luo UDP-socketti, jota käytetään kommunikointiin palvelimien kanssa.
+  self.sock = udp.createSocket('udp4');
   // Kuuntele pong-paketteja
-  self.sock.on('message', function (data, peer) {
-    var srv;
+  self.sock.bind(up, ua);
+  self.pingPacket = new Buffer([0x67, 0x73, 0x73, 0x20, 0x50, 0x49, 0x4e, 0x47]);
+  self.sock.on('message', function onUDP(data, peer) {
     if (String(data.slice(data.length - 4)) === 'PONG') {
-      for (var i = self.servers.length; i--;) {
-        var srv = self.servers[i];
+      self.servers.forEach(function serverLoop(srv) {
         if (srv.ip === peer.address && srv.port === peer.port) {
           srv.ping = Date.now() - srv.pingT;
         }
-      }
+      });
     }
   });
 
-  var file = new (static.Server)();
-
+  // Luodaan http-palvelin, ja kuunnellaan.
   self.htp = http.createServer().listen(p, a);
-  self.htp.on('request', function (req, res) {
+
+  // Käsitellään pyynnöt.
+  self.htp.on('request', function onHTTP(req, res) {
     var uri = url.parse(req.url, true)
       , mode = uri.query.mode
       , location = uri.path.split('/');
 
-    // Pyyntö on rekisteröinti-apille
     if (location[1] === 'reg') {
-      // Palautetaan aina selkokieltä
+      // Pyydettiin jotain rekisteröintiin liittyvää.
       res.writeHead(200, {'Content-Type': 'text/plain'});
-
       // Mitä tehdään?
       if (mode && ['reg', 'list', 'unreg', 'update'].indexOf(mode) !== -1) {
         self.emit(mode, req, res, uri);
@@ -61,51 +76,93 @@ function GSS(p, a) {
         res.end('unknown_mode');
       }
     } else if (location[1] === 'assets') {
-      // Tarjotaan staattisia varoja
-      file.serve(req, res);
+      // Pyydettiin jotain assets-kansiosta.
+      assets.deliver(__dirname, req, res);
+    } else if (location[1] === 'log') {
+      // Pyydettiin lokia.
+      self.emit('log', req, res, location);
     } else {
-
-      self.servers.map(function (s) {
-        // Päivitetään pingit
-        self.getUpdatePing(s);
-      });
-
-      self.listingTempl = hogan.compile(fs.readFileSync('listing.html', 'utf8'));
-      res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-      res.end(self.listingTempl.render({
-        servers: self.servers.filter(function (s) { return s.active; })
-      }));
+      // Muissa tapauksissa annetaan vain html-listaus servuista.
+      self.emit('html', req, res);
     }
   });
 }
-// Laajennetaan EventEmitteriä, saadaan emit.
-GSS.prototype.__proto__ = process.EventEmitter.prototype;
 
-GSS.prototype.getServer = function (sid) {
+// Laajennetaan EventEmitteriä, saadaan emit.
+util.inherits(GSS, process.EventEmitter);
+
+GSS.prototype.getServer = function (sid, cb) {
   for (var i = this.servers.length; i--;) {
-    var s = this.servers[i];
-    if (s.addr + ':' + s.port === sid) {
-      return s;
-    }
-  }
+    if (this.servers[i].sid === sid) { return this.servers[i]; }
+  };
 };
 
 GSS.prototype.getUpdatePing = function (srv) {
   if (srv && parseFloat(srv.ver.slice(1)) >= 2.5) {
     srv.pingT = Date.now();
-    var msg = Buffer(8);
-    msg.writeUInt32LE(544437095, 0);
-    msg.write('PING', 4, 4);
-    // [ ,->  544437095  <-.  |  ,->    PING    <--.  ]
-    gss.sock.send(msg, 0, 8, srv.port, srv.addr);
+    this.sock.send(this.pingPacket, 0, 8, srv.port, srv.addr);
   }
-}
+};
 
-process.on('uncaughtException', function (e) {
-  console.log(e.stack);
-});
+GSS.prototype.endRequest = function (res, log, msg) {
+  res.end(msg); // Vastataan http-pyyntöön
+  log.end(msg, msg === 'ok'); // Kirjoitetaan vastaus myös lokiin
+};
 
 var gss = new GSS(argv.p, argv.a);
+
+/***************\
+| HTML LOKI :O) |
+\************ **/
+gss.on('log', function onHTMLLog(req, res, location) {
+  var self = this
+    , currFile = location[2] || 'gss.log'
+
+  // Debug
+  self.logTempl = hogan.compile(fs.readFileSync('log.html', 'utf8'));
+
+  // Hoidetaan IO asynkronisesti!
+  async.parallel(
+  { log: function readLog(cb) {
+      fs.readFile('./logs/' + currFile, 'utf8', cb); }
+  , files: function listLogs(cb) {
+      fs.readdir('./logs', cb); }
+  }, // Valmista
+  function fileIODone(err, results) {
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    if (err) { res.end('error'); return; }
+    var logs = results.log.split('\n')
+      , json = '[' + logs.slice(0, logs.length - 1) + ']'
+      , lobj = JSON.parse(json)
+      , files = results.files.map(function (f) {
+          if (f === currFile) { return '<a class="this" href="./log/' + f + '">' + f + '</a>'; }
+          else                { return '<a              href="./log/' + f + '">' + f + '</a>'; }
+        });
+    // Tarjoillaan hienosti muotoiltu loki.
+    res.end(self.logTempl.render({files: files, logs: lobj, currentFile: currFile}));
+  });
+});
+
+/******************\
+| HTML LISTAUS :O) |
+\******************/
+gss.on('html', function onHTMLList(req, res) {
+  var self = this;
+
+  // Päivitetään pingit
+  self.servers.forEach(function (s) {
+    self.getUpdatePing(s);
+  });
+
+  // Debug käytössä ladataan template aina uusiksi
+  //self.listingTempl = hogan.compile(fs.readFileSync('listing.html', 'utf8'));
+
+  res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+  res.end(self.listingTempl.render({
+    // Listataan ainoastaan aktiiviset palvelimet
+    servers: self.servers.filter(function (s) { return s.active; })
+  }));
+});
 
 /**************************|*******************.
 | SERVUN REKISTERÖINTI :O) |                    \
@@ -117,52 +174,60 @@ var gss = new GSS(argv.p, argv.a);
 |                        portilla on jo palvelin /
 |   ok                 = kaikki onnistui        /
 `**********************************************/
-gss.on('reg', function (req, res, uri) {
+gss.on('reg', function onRegister(req, res, uri) {
   var self = this
     , q = uri.query
     , addr   = q.addr || req.headers['x-forwarded-for']
     , port = parseInt(q.port, 10)
-    , sid  = addr + ':' + port
-    , serv = gss.getServer(sid)
-    , devb = !!q.devbuild;
+    , sid = addr + ':' + port
+    , serv = self.getServer(sid)
+    , log = new Logger(sid);
+
+  log.gss('register', req.headers, q);
 
   // Jokin tiedoista puuttuu
   if (!q.profile || !q.ver || !q.desc || !port) {
-    res.end('registering_failed');
+    log.add('Missing obligatory information from parameters.');
+    self.endRequest(res, log, 'registering_failed');
     return;
   }
 
   // Jokin tiedoista näyttää alustavasti väärältä
   if (q.profile !== NM_PROFILE || !q.desc.length || port < 1 || port > 65535) {
-    res.end('registering_failed');
+    log.add('Incorrect parameters.');
+    self.endRequest(res, log, 'registering_failed');
     return;
   }
 
   // Katsotaan löytyyko palvelin jo samasta osoitteesta ja portista
   if (serv && Date.now() - serv.last < 90000) {
-    res.end('server_exists');
+    self.endRequest(res, log, 'server_exists');
     return;
-  } else if (serv) {
-    self.servers.splice(self.servers.indexOf(serv), 1);
   }
+  // Poistetaan serveri, jos sellainen on.
+  if (serv) { self.servers.splice(self.servers.indexOf(serv), 1); }
 
   // Tarkistetaan serverin toimivuus UDP-paketilla.
   //                       [ ,->  544437095  <-.  |  ,->    GSS+    <--.  ]
-  gss.sock.send(new Buffer([0x67, 0x73, 0x73, 0x20, 0x47, 0x53, 0x53, 0x2B]), 0, 8, port, addr,
+  self.sock.send(new Buffer([0x67, 0x73, 0x73, 0x20, 0x47, 0x53, 0x53, 0x2B]), 0, 8, port, addr,
   function sockSend(err, bytes) {
     if (err) {
-      res.end('system_error');
+      log.add('Socket error.');
+      log.add(err);
+      self.endRequest(res, log, 'system_error');
       return;
     }
 
     serv = {
       active: false,
+      sid: sid,
       addr: addr,
       port: port,
       ver: q.ver,
       desc: q.desc,
-      dev: devb,
+      dev: !!q.devbuild,
       ip: addr,
+      log: log,
       last: undefined
     };
 
@@ -172,10 +237,10 @@ gss.on('reg', function (req, res, uri) {
     // Muunnetaan osoite ip:ksi, jotta sitä voidaan verrata saapuviin UDP-paketteihin.
     dns.resolve(addr, function (err, ip) {
       if (!err) { serv.ip = ip[0]; }
-    })
+    });
   });
 
-  res.end('ok')
+  self.endRequest(res, log, 'ok');
 });
 
 /**************************|*******************.
@@ -186,37 +251,45 @@ gss.on('reg', function (req, res, uri) {
 |   system_error       = MySQL feilasi           /
 |   ok                 = kaikki onnistui        /
 `**********************************************/
-gss.on('update', function (req, res, uri) {
-  var self = this, info
+gss.on('update', function onUpdate(req, res, uri) {
+  var self = this, info, log
   , q = uri.query
   , addr   = q.addr || req.headers['x-forwarded-for']
   , port = parseInt(q.port, 10)
-  , sid  = addr + ':' + port
-  , serv = gss.getServer(sid)
-  , devb = !!q.devbuild;
+  , sid = addr + ':' + port
+  , serv = self.getServer(sid)
+
+  // Onko servu rekisteröity?
+  if (!serv) {
+    // Luodaan uusi loki.
+    log = new Logger(sid, 'a');
+    log.gss('update', req.headers, q);
+    log.add('Server does not exist.');
+    self.endRequest(res, log, 'update_failed');
+    return;
+  }
+
+  serv.log.gss('update', req.headers, q);
 
   // Jokin tiedoista puuttuu
   if (!q.profile || !q.port || !q.data) {
-    console.log('insufficient parameters.');
-    res.end('update_failed');
+    serv.log.add('Insufficient parameters.');
+    self.endRequest(res, serv.log, 'update_failed');
     return;
   }
   // Jokin tiedoista näyttää alustavasti väärältä
   if (q.profile !== NM_PROFILE || !q.data.length || port < 1 || port > 65535) {
-    res.end('update_failed');
-    return;
-  }
-  // Onko servu rekisteröity?
-  if (!serv) {
-    res.end('update_failed');
+    serv.log.add('Incorrect parameters.');
+    self.endRequest(res, serv.log, 'update_failed');
     return;
   }
 
   // Päivitetään pingi, jos servu on tarpeeksi uusi
   self.getUpdatePing(serv);
-  
+
   info = q.data.split(',');
   // Update data
+  serv.infoString = q.data;
   serv.info = {
     players: parseInt(info[0], 10),
     bots: parseInt(info[1], 10),
@@ -224,9 +297,10 @@ gss.on('update', function (req, res, uri) {
     maxPlayers: parseInt(info[3], 10),
     names: info[4] ? '<em>' + join(info[4].split('|'), '</em>, <em>', '</em>and <em>') + '</em>' : ''
   };
+
   serv.active = true;
   serv.last = Date.now();
-  res.end('ok');
+  self.endRequest(res, serv.log, 'ok');
 });
 
 /*********************************|************.
@@ -236,39 +310,93 @@ gss.on('update', function (req, res, uri) {
 |   unregistering_failed = tiedot väärin         /
 |   ok                   = kaikki onnistui      /
 `**********************************************/
-gss.on('unreg', function (req, res, uri) {
+gss.on('unreg', function onUnregister(req, res, uri) {
   var self = this, info
   , q = uri.query
   , addr   = q.addr || req.headers['x-forwarded-for']
   , port = parseInt(q.port, 10)
-  , sid  = addr + ':' + port
-  , serv = gss.getServer(sid)
-  , devb = !!q.devbuild;
+  , sid = addr + ':' + port
+  , serv = self.getServer(sid)
+
+  // Onko servu rekisteröity?
+  if (!serv) {
+    log = new Logger(sid, 'a');
+    log.gss('unregister', req.headers, q);
+    log.add('server does not exist');
+    self.endRequest(res, log, 'unregistering_failed');
+    return;
+  }
+
+  serv.log.gss('unregister', req.headers, q);
+
+  self.getServer(addr + ':' + port, function (s) { serv = s; });
 
   // Jokin tiedoista puuttuu
   if (!q.profile || !q.port) {
-    console.log('insufficient parameters.');
-    res.end('unregistering_failed');
+    serv.log.add('insufficient parameters');
+    self.endRequest(res, serv.log, 'unregistering_failed');
     return;
   }
   // Jokin tiedoista näyttää alustavasti väärältä
   if (q.profile !== NM_PROFILE || port < 1 || port > 65535) {
-    res.end('unregistering_failed');
-    return;
-  }
-  // Onko servu rekisteröity?
-  if (!serv) {
-    res.end('unregistering_failed');
+    serv.log.add('incorrect parameters');
+    self.endRequest(res, serv.log, 'unregistering_failed');
     return;
   }
 
   self.servers.splice(self.servers.indexOf(serv), 1);
-  res.end('ok');
+  self.endRequest(res, serv.log, 'ok');
 });
 
 gss.on('list', function (req, res, uri) {
-  res.end('GSS:');
+  var self = this, serv
+  , q = uri.query
+  , debug = !!q.debug
+  , dev = !!q.devbuild
+  , list = 'GSS:';
+
+  // Jos osoitteessa on debug-parametri, niin ei tarkisteta muita.
+  if (!debug) {
+    if (q.profile !== NM_PROFILE || !q.ver) {
+      self.log.gss('list', req.headers, q);
+      self.log.add('incorrect parameters');
+      self.endRequest(res, self.log, 'listing_failed');
+    }
+  }
+
+  // Luupataan palvelimet
+  self.servers.forEach(function (serv, i) {
+
+    // Onko palvelin aktiivinen?
+    if (Date.now() - serv.last > 90000) { serv.active = false; }
+    // Jos ei niin poistetaan listauksesta.
+    if (!serv.active) {
+      serv.log.info('timeout', 'Deleted server after inactivity.');
+      serv.log.end('deleted');
+      self.servers.splice(i, 1);
+      return;
+    }
+
+    // Tulostetaan, jos versiot täsmää, klientti on dev tai debug-tila on päällä.
+    if ((serv.ver === q.ver && !serv.dev) || dev || debug) {
+      list += 'name=' + serv.desc + '|'
+           +  'addr=' + serv.addr + '|'
+           +  'port=' + serv.port + '|'
+           +  'info=' + serv.infoString + '|'
+           +  'ver='  + serv.ver + (serv.dev && debug ? '-dev' : '') + '\n';
+    }
+
+  });
+
+  res.end(list);
 });
 
+/** Käsitellään ei napatut virheet, jotta palvelin ei kaatuisi. */
+process.on('uncaughtException', function (e) {
+  // Varmuuden vuoksi tulostetaan konsoliin, jos loggeri kaatuu. :E
+  console.log(e.stack);
+  gss.log.error('uncaughtException', e);
+  gss.log.end('fix!', false);
+});
 
 function each(obj, fn) { Object.keys(obj).forEach(function (n) { fn(obj[n]); }); }
---
+++ gss/Logger.js
@@ -0,0 +1,103 @@
+var util = require('util')
+  , fs = require('fs');
+
+/** Logger */
+function Logger(sid, flag) {
+  // Login id (server id = address:port)
+  this.sid = sid;
+  // Striimi johon kirjoitetaan
+  this.out = fs.createWriteStream(
+    __dirname + '/logs/' + sid.replace(/:/g, '-') + '.log', {flags: flag || 'w'});
+  // Tähän kasataan viesti, joka kirjoitetaan sitten flush-funktiolla.
+  this.buf = {};
+  // Viimeisin viesti
+  this.last = {};
+}
+
+// Luo uuden gss-loki-objektin
+Logger.prototype.gss = function (type, req, params) {
+  // Luodaan viesti
+  this.buf = {
+    name: type,
+    time: Date.now(),
+    date: new Date().toUTCString(),
+    from: this.sid,
+    details: [
+      {name: 'Request', msg: JSON.stringify(req, null, '  ')},
+      {name: 'Parameters', msg: JSON.stringify(params, null, '  ')}
+    ]
+  };
+  // Update ja samat parametrit niin ei kirjoiteta lokiin turhaa spämmiä.
+  if (this.buf.name === 'update' && this.last.name === 'update' &&
+      this.buf.details[0].msg === this.last.details[0].msg &&
+      this.buf.details[1].msg === this.last.details[1].msg) {
+    this.buf = undefined;
+  }
+}
+
+Logger.prototype.error = function (name, e) {
+  this.buf = {
+    name: name,
+    time: Date.now(),
+    date: new Date().toUTCString(),
+    from: 'process',
+    details: [
+      {'name': 'Stack', msg: e.stack}
+    ]
+  };
+};
+
+Logger.prototype.info = function (name, message) {
+  this.buf = {
+    name: name,
+    time: Date.now(),
+    date: new Date().toUTCString(),
+    from: this.sid,
+    details: [
+      {'name': 'Message', msg: message}
+    ]
+  };
+}
+
+/** Lisätään lisätietoja */
+Logger.prototype.add = function (msg) {
+  if (!this.buf.details) { this.buf.details = []; }
+  if (msg instanceof Error) {
+    // Lisätään virheen kasa
+    this.buf.details.push({
+      name: 'Stack',
+      msg: msg.stack
+    });
+  } else {
+    // Lisätään viesti
+    this.buf.details.push({
+      name: 'Details',
+      msg: msg
+    });
+  }
+};
+
+Logger.prototype.end = function (msg, status) {
+  // Ei lopetella tyhjiä viestejä.
+  if (!this.buf) { return; }
+  if (status) {
+    this.buf.end = '<span class="sucs">' + msg + '</span>';
+    this.success = true;
+  } else {
+    this.buf.end = '<span class="fail">' + msg + '</span>';
+    this.success = false;
+  }
+  // Kirjoitetaan loki.
+  this.flush();
+};
+
+/** Kirjoitetaan loki striimiin */
+Logger.prototype.flush = function () {
+  // Ei kirjoiteta, jos tyhjä.
+  if (!this.buf) { return; }
+  this.out.write(JSON.stringify(this.buf) + '\n');
+  this.last = this.buf;
+  this.buf = {};
+};
+
+exports.Logger = Logger;
